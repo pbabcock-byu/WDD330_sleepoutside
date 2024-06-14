0b48b57c049472c8d785505537b23200c1ee15bc
Orlando Ancelmo Pinelo <orlandoancelmopinelo@gmail.com>
2024-06-13 23:57:38 -0600

    Modified file product.js , function addProductToCart , imported getLocalStorage.

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let currentCart = getLocalStorage("so-cart"); //retrieve from LS
+  if (!Array.isArray(currentCart)) {
+    //is it an array
+    currentCart = [];
+  }
+  currentCart.push(product); //add to cart
+  setLocalStorage("so-cart", currentCart); // save to LS
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
